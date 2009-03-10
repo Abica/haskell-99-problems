282623b0875e50222c7e36a65b99802c470eadcb
Nicholas Wright <nick.loves.rails@gmail.com>
2009-03-09 18:13:45 -0600

    use proper haskell naming conventions for lists add spaces to typings so they don't annoy me

--- 1-5.hs
+++ 1-5.hs
@@ -1,25 +1,25 @@
 -- Problem 1
 -- Find the last element of a list.
 myLast :: [a] -> a
-myLast x = (head . reverse) x
+myLast xs = (head . reverse) xs
 
 -- Problem 2
 -- Find the last but one element of a list. 
 myButLast :: [a] -> a
-myButLast x = reverse x !! 1
+myButLast xs = reverse xs !! 1
 
 -- Problem 3
 -- Find the K'th element of a list. The first element in the list is number 1. 
-elementAt ::[a] -> Int -> a
-elementAt x i = x !! (i - 1)
+elementAt :: [a] -> Int -> a
+elementAt xs i = xs !! (i - 1)
 
 -- Problem 4
 -- Find the number of elements of a list. 
-myLength ::[a] -> Int
+myLength :: [a] -> Int
 myLength [] = 0
 myLength (_:xs) = 1 + myLength xs
 
 -- Problem 5
 -- Reverse a list. 
 reverse' :: [a] -> [a]
-reverse' x = foldl (\acc n -> n : acc) [] x
+reverse' xs = foldl (\acc n -> n : acc) [] xs
